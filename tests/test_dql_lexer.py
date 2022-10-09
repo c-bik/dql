1ae4e0507d7387b92d9329cf43dd1f767e7530c8
Bikram Chatterjee <bikram@konnexions.ch>
2022-10-09 09:27:50 +0200

    infinite decision branching AST

--- tests/test_dql_lexer.py
+++ tests/test_dql_lexer.py
@@ -2,75 +2,86 @@ from pprint import pprint
 
 from dql.dql_lex import DqlLex
 from dql.dql_yac import parser
-
-input_string = """
-TYPE IS "FOO"
-VALUE IS "bar" WITHIN MAX 3
-(
-    TYPE IN ("FOO", "BAR", "BAZ")
-    OR VALUE IN ("foo", "bar", "baz")
-) WITHIN MAX 5
-"""
-
-
-def test_lexer():
-    dq = DqlLex()
-    dq.tokenize(input_string=input_string)
-
-    expected = [
-        ('TYPE', 'TYPE'),
-        ('IS', 'IS'),
-        ('STRING', 'FOO'),
-        ('VALUE', 'VALUE'),
-        ('IS', 'IS'),
-        ('STRING', 'bar'),
-        ('WITHIN', 'WITHIN'),
-        ('MAX', 'MAX'),
-        ('NUMBER', 3),
-        ('(', '('),
-        ('TYPE', 'TYPE'),
-        ('IN', 'IN'),
-        ('(', '('),
-        ('STRING', 'FOO'),
-        (',', ','),
-        ('STRING', 'BAR'),
-        (',', ','),
-        ('STRING', 'BAZ'),
-        (')', ')'),
-        ('OR', 'OR'),
-        ('VALUE', 'VALUE'),
-        ('IN', 'IN'),
-        ('(', '('),
-        ('STRING', 'foo'),
-        (',', ','),
-        ('STRING', 'bar'),
-        (',', ','),
-        ('STRING', 'baz'),
-        (')', ')'),
-        (')', ')'),
-        ('WITHIN', 'WITHIN'),
-        ('MAX', 'MAX'),
-        ('NUMBER', 5)
-    ]
-
-    actual = [
-        (tok.type, tok.value)
-        for tok in dq.lexer
-    ]
-
-    assert expected == actual
+from dql.dql_engine import DqlEngine
 
 
 def test_parser():
     dq = DqlLex()
+    dql = """
+    TYPE IS "FOO"
+    VALUE IS "bar" BEFORE 3
+    SKIP 10
+    (
+        (
+            TYPE IN ("FOO", "BAR", "BAZ")
+            (
+                TYPE IN ("FOO1", "BAR1", "BAZ1")
+                OR (
+                    VALUE IN ("foo", "bar", "baz")
+                    VALUE IN ("foo1", "bar1", "baz1")
+                )
+                OR (
+                    VALUE IN ("foo2", "bar2", "baz2")
+                    VALUE IN ("foo3", "bar3", "baz3")
+                )
+            ) BETWEEN 3 5
+        )
+        OR (
+            VALUE IN ("foo", "bar", "baz")
+            VALUE IN ("foo1", "bar1", "baz1")
+        )
+        OR (
+            VALUE IN ("foo2", "bar2", "baz2")
+            VALUE IN ("foo3", "bar3", "baz3")
+        )
+    ) AFTER 5
+    """
+
+    rule = parser.parse(dql, lexer=dq.lexer)
+
+    expected_rule = [
+        ('type', 'FOO'),
+        ('before', 3, ('value', 'bar')),
+        ('skip', 10),
+        ('after', 5,
+         ('or', [
+             [
+                 ('type', ['FOO', 'BAR', 'BAZ']),
+                 ('between', 3, 5,
+                  ('or', [
+                      ('type', ['FOO1', 'BAR1', 'BAZ1']),
+                      [
+                          ('value', ['foo', 'bar', 'baz']),
+                          ('value', ['foo1', 'bar1', 'baz1'])
+                      ],
+                      [
+                          ('value', ['foo2', 'bar2', 'baz2']),
+                          ('value', ['foo3', 'bar3', 'baz3'])
+                      ]
+                  ]))
+             ],
+             [
+                 ('value', ['foo', 'bar', 'baz']),
+                 ('value', ['foo1', 'bar1', 'baz1'])
+             ],
+             [
+                 ('value', ['foo2', 'bar2', 'baz2']),
+                 ('value', ['foo3', 'bar3', 'baz3'])
+             ]
+         ]))
+    ]
+    assert expected_rule == rule
+
+
+def test_engine():
+    dq = DqlLex()
     input_string1 = """
     TYPE IS "FOO"
-    VALUE IS "bar" WITHIN MAX 3
-    (
-        TYPE IN ("FOO", "BAR", "BAZ")
-        OR VALUE IN ("foo", "bar", "baz")
-    ) WITHIN MAX 5
+    VALUE IS "bar" BEFORE 3
+    SKIP 1
+    TYPE IS "d" AFTER 1
     """
-    p = parser.parse(input_string1, lexer=dq.lexer)
-    print()
-    pprint(p)
+    rules = parser.parse(input_string1, lexer=dq.lexer)
+
+    tokens = [('FOO', 1), ('FOO', "bar"), ('b', 2), ('d', 5)]
+    assert not DqlEngine(rules).match(tokens)
